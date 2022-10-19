234d0828915e082124e6c202a9733ab528095518
Tap <103682757+TapLua@users.noreply.github.com>
2022-10-19 09:30:11 +0100

    Create Loader.lua

---
+++ Scripts/Loader.lua
@@ -0,0 +1,13 @@
+repeat task.wait() until game:IsLoaded()
+
+if game.PlaceId == 7026949294 then --Sword Sim
+
+elseif game.PlaceId == 3956818381 then --Ninja Legends
+
+elseif game.PlaceId == 9498006165 then --Tapping Simulator
+
+elseif game.PlaceId == 8726743209 then --Refinery Caves
+
+else
+    game.Players.LocalPlayer:Kick("Game not supported by Arches Systems.")
+end
